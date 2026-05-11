c7fefa76270b85a60bc576c557e31f221753698f
George Edward <gmedward2002@outlook.com>
2026-05-11 10:38:29 +0200

    update

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -11,36 +11,6 @@ def plot_reg(df):
     plt.plot(x,y,lw=1.6)
 
 
-
-def simulate_data(seed=99):
-    np.random.seed(seed)
-    
-    severity_levels = [
-        "Mild",
-        "Moderate",
-        "Severe",
-    ]
-    n_per_group = 100
-    data = []
-
-    for i, sev in enumerate(severity_levels):
-        
-        # dosage increases with severity
-        dosage = np.random.normal(loc=i+6, scale=1.2, size=n_per_group)
-
-        # higher dosage -> faster recovery
-        recovery_time = np.random.normal(7 + 2.5*i, 1.1, n_per_group) - 0.35*dosage
-
-        # ensure reasonable bounds
-        dosage = np.clip(dosage, 0.2, 10)
-        recovery_time = np.clip(recovery_time, 0.0, None)
-        
-        for x, y in zip(dosage, recovery_time):
-            data.append([x, y, sev])
-
-    return pd.DataFrame(data, columns=["dosage", "recovery_time", "severity"])
-
-
 ######################################################################################################
 from causallearn.graph.GraphNode import GraphNode
 from causallearn.graph.GeneralGraph import GeneralGraph
